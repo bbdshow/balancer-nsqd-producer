fb738c9a91201e6740a4742c6424b1150e7ae95c
hopingtop <hopingtopmail@gmail.com>
2018-10-04 10:50:33 +0800

    remove coveragealls

--- coverage.sh
+++ coverage.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
 set -e
-
-echo "mode: count" > coverage.out
-
 for d in $(go list ./... | grep -E 'balancer-nsqd-producer$|algorithm$'); do
-    go test -v -covermode=count -coverprofile=profile.out $d
-    if [ -f profile.out ]; then
-        cat profile.out | grep -v "mode:" >> coverage.out
-        rm profile.out
-    fi
+    go test -v -covermode=count -coverprofile=coverage.out $d
 done
 
-echo "mode: goveralls" > profile.out
-$GOPATH/bin/goveralls -coverprofile="profile.out" \
-                          -service=travis-ci    \
-                          -ignore="example" || true
-rm profile.out
+for d in $(go list ./... | grep -E 'balancer-nsqd-producer$|algorithm$'); do
+   go test -race -covermode=atomic -coverprofile=coverage.txt 
+done
+
